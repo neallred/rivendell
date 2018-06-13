499f6e4d68532759eb89289a10e0eeedc6d7f13a
Nathaniel Allred <nathaniel.allred@leadpages.net>
2018-06-13 12:16:08 -0500

    add vim bootstrap script

---
+++ .utils/bootstrap-vim.sh
@@ -0,0 +1,13 @@
+# PATHOGEN
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+cd ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/ElmCast/elm-vim.git
+git clone https://github.com/FooSoft/vim-argwrap.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/mhartington/oceanic-next.git
+git clone https://github.com/mxw/vim-jsx
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/w0rp/ale.git
