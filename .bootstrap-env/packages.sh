6778ec467b5e4ec5e28e0d6e70b5417e3db572d0
Nathaniel Allred <neallred@gmail.com>
2018-10-07 10:28:21 -0500

    update package manager

--- .bootstrap-env/packages.sh
+++ .bootstrap-env/packages.sh
@@ -32,38 +32,43 @@ INSTALL_LIST_MAC_GUI=" \
 OS=`uname -s`
 PACKAGE_MANAGER=""
 PACKAGE_INSTALL=""
+PACKAGE_MANAGER_UPDATE=""
 
 set_package_manager() {
   if command -v apt-get >/dev/null 2>&1; then
     PACKAGE_MANAGER="apt-get"
     PACKAGE_INSTALL="sudo apt-get install"
+    PACKAGE_MANAGER_UPDATE="sudo apt-get update"
 
   elif command -v pacman >/dev/null 2>&1; then
     PACKAGE_MANAGER="pacman"
     PACKAGE_INSTALL="sudo pacman -S"
+    PACKAGE_MANAGER_UPDATE="sudo pacman -Syu"
 
   elif command -v brew >/dev/null 2>&1; then
     PACKAGE_MANAGER="brew"
     PACKAGE_INSTALL="brew install"
+    PACKAGE_MANAGER_UPDATE="brew update"
 
   elif command -v yum >/dev/null 2>&1; then
     PACKAGE_MANAGER="yum"
     PACKAGE_INSTALL="sudo yum install"
+    PACKAGE_MANAGER_UPDATE="sudo yum update"
 
   elif command -v nix-env >/dev/null 2>&1; then
     PACKAGE_MANAGER="nix-env"
     PACKAGE_INSTALL="nix-env install"
+    PACKAGE_MANAGER_UPDATE="nix-channel --update nixpkgs"
 
   else
     PACKAGE_MANAGER="unknown"
+
   fi
 }
 
-echo "MY PACKAGE INSTALL COMMAND"
 set_package_manager
 
-
-echo $PACKAGE_INSTALL
+$PACKAGE_MANAGER_UPDATE
 
 $PACKAGE_INSTALL $INSTALL_LIST_ALL
 
