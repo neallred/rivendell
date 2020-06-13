9b17cdc574336cf1aed8d2ac246280791b4a1a69
neallred <neallred@gmail.com>
2020-06-12 20:47:27 -0500

    Config updates

---
+++ .config/nvim/local-repos.vim
@@ -0,0 +1,6 @@
+nnoremap <Leader>oa :call Notrw($p_archery)<CR>
+nnoremap <Leader>ol :call Notrw($p_allredlib)<CR>
+nnoremap <Leader>os :call Notrw($p_scriptured)<CR>
+nnoremap <Leader>ot :call Notrw($p_t)<CR>
+" nnoremap <Leader>og :call Notrw($p_go)<CR>
+nnoremap <Leader>og :call Notrw($p_general_conference)<CR>
--- .utils/git/status-all.sh
+++ .utils/git/status-all.sh
@@ -1,9 +1,16 @@
+# Needed if repo remotes use ssh and need to be checked for remote HEAD
+if [ -f "$HOME/.ssh/id_rsa" ]; then
+  if [ -z "$(ssh-add -L)" ]; then
+    ssh-add -q $HOME/.ssh/id_rsa
+  fi
+fi
 for c in $PROJECT_CONTAINERS
 do
   echo CONTAINER $c
   repos=$(ls -d $c/*/)
   for r in $repos
   do
+    declare -a msgs=()
     pushd $r > /dev/null
     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null ) != "true" ]]; then
       echo $r not a repo
@@ -12,14 +19,26 @@ do
     fi
     branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null )
     if [[ $branch != "master" ]]; then
-      echo $r at $branch
+      # Additional slow check for rarer case that remote HEAD is
+      # something other than "master"
+      master_branch=$(git remote show origin | grep HEAD | grep -o "\S*\$")
+      if [[ $branch != $master_branch ]]; then
+        msgs+=("is on branch $branch")
+      fi
     fi
     if [ -z "$(git status --porcelain)" ]; then 
       # Working directory clean
       :
     else 
-      # Working directory clean
-      echo "$r" has changes 
+      msgs+=("has changes")
+    fi
+    msgslength=${#msgs[@]}
+    if [ "$msgslength" -gt "0" ]; then
+      echo "$r:"
+      for (( i=1; i<${msgslength}+1; i++ ));
+      do
+        echo "  * ${msgs[$i-1]}"
+      done
     fi
     popd > /dev/null
   done
